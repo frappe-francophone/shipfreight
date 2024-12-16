aace20d5a2a0c1d160fa2e85502230d500346583
frappe-francophone <frappe-francophone@gmail.com>
2024-12-16 18:54:30 +0200

    mise à de la table tarif Société

---
+++ shipfreight/ship_freight/doctype/ville_ship/test_ville_ship.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Ebama Dernis and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestVilleShip(FrappeTestCase):
+	pass
