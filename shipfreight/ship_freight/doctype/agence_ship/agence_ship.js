5d862673a9e8857992bf0d3ad44d934638e70c3b
frappe-francophone <frappe-francophone@gmail.com>
2024-12-17 18:43:01 +0200

    creation de la table agence et moyen transport

---
+++ shipfreight/ship_freight/doctype/agence_ship/agence_ship.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Ebama Dernis and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Agence Ship", {
+// 	refresh(frm) {
+
+// 	},
+// });
